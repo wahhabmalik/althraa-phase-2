403506c8c135ae3bc1c0df227d516ea4d8ccf54c
wahhabmalik <malik.wahhab@gmail.com>
2020-05-20 23:18:31 +0500

    v2 report page 8 fix

--- resources/views/dashboard/pdf/report.blade.php
+++ resources/views/dashboard/pdf/report.blade.php
@@ -1143,8 +1143,6 @@
 	<br><br><br><br><br>
 	<br><br><br><br><br>
 	<br><br><br><br><br>
-	<br><br><br><br><br>
-	<br><br><br><br><br>
 	<p class="text-center mr-5">{{ $data['credits'] }}</p>
 	</div>
 
