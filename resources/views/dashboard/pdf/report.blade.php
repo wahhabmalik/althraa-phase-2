f69ceab75baea9b1aa1d0a2ef548438fb50e6a7b
wahhabmalik <malik.wahhab@gmail.com>
2020-06-09 20:21:18 +0500

    final deployment

--- resources/views/dashboard/pdf/report.blade.php
+++ resources/views/dashboard/pdf/report.blade.php
@@ -1529,6 +1529,25 @@ $(document).ready(function(){
 	3000);
 });
 
+$(document).ready(function(){
+	setTimeout(
+		function() {
+			function myFunction(x) {
+
+				if (x.matches) { // If media query matches
+				    document.body.style.backgroundColor = "yellow";
+				 } else {
+				   document.body.style.backgroundColor = "pink";
+				  }
+				}
+
+				var x = window.matchMedia("(max-width: 700px)")
+				myFunction(x) // Call listener function at run time
+				x.addListener(myFunction) // Attach listener function on state changes
+		},
+	8000);
+});
+
 </script>
 
 @endsection
