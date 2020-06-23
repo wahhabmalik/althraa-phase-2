8e6c89ceff23a140fea6f904870f327dc45a15b4
wahhabmalik <malik.wahhab@gmail.com>
2020-06-23 21:10:55 +0500

    .

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -1060,7 +1060,7 @@ return [
 
     'beta_version' => 'beta version',
 
-    'New_Code_has_been_sent_to_you' => 'New Code has been sent to you'
+    'New_Code_has_been_sent_to_you' => 'New Code has been sent to you',
 
     'Verification_failed_please_try_again_or_try_new_code' => 'Verification failed please try again or try new code',
 
