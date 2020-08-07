96a3e4de2d8be182d3a1ad47104c3ddc1b817296
wahhabmalik <malik.wahhab@gmail.com>
2020-08-07 10:47:54 +0500

    email fixes

--- app/User.php
+++ app/User.php
@@ -115,6 +115,7 @@ class User extends Authenticatable
 
             $status = array('msg' => "2F Auth Expired. You can not login at this time due to some technical issues. Consult Admin for further inquiries.", 'toastr' => "errorToastr");
             Session::put('error', $e->getMessage());
+            
             return redirect('/en/login');
         }
     }
